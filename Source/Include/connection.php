3b9907e8b270564bd57566972e5779b520d6a390
quangthang1 <44992434+quangthang1@users.noreply.github.com>
2018-12-20 23:20:57 +0700

    Add files via upload

--- Source/Include/connection.php
+++ Source/Include/connection.php
@@ -3,7 +3,8 @@ $server_username = "root"; // thông tin đăng nhập host
 $server_password = ""; // mật khẩu, trong trường hợp này là trống
 $server_host = "localhost"; // host là localhost
 $database = 'db_web'; // database là website
- 
+
+
 // Tạo kết nối đến database dùng mysqli_connect()
 $conn = mysqli_connect($server_host,$server_username,$server_password,$database) or die("không thể kết nối tới database");
 // Thiết lập kết nối ủa chúng ta khi truy vấn là dạng UTF8 trong trường hợp dữ liệu là tiếng việt có dâu
