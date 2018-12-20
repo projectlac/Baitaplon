f4286ed1d89f93af2d78e0e51eabe69e4f5ac855
quangthang1 <44992434+quangthang1@users.noreply.github.com>
2018-12-20 23:46:55 +0700

    Add files via upload

--- Source/Download/Xuly.php
+++ Source/Download/Xuly.php
@@ -1,12 +1,16 @@
 <?php
-$fname=$_GET["filename"];
-// mở file
-$file = fopen("filedown/".$fname,"rb");
-//Báo dữ liệu là nhị phân
-header("Content-Type:application/octet-stream");
 
-header("content-Disposition: attachment;filename=".$fname); // nếu ko có sẽ down file xuly.php
-//đọc và hiển thị
-fpassthru($file);
-fclose($file);
+if($_FILES["userfile"]["error"]>0){
+    echo "File chưa được chọn hoặc kích thước file quá 2MB"; // kích cỡ file
+}
+else{
+    if($_FILES["userfile"]["type"]!= "image/jpeg" && $_FILES["userfile"]["type"]!= "image/png"){
+    echo "Bạn chỉ được upload file .jpeg hoặc .png";
+    }
+    else{
+    move_uploaded_file($_FILES["userfile"]["tmp_name"],"data/".$_FILES["userfile"]["name"]);
+    echo " Upload Thành Công";
+    }
+
+}
 ?>
